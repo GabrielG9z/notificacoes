77c529406354619b18ab48641f7b8f355a05d986
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2023-01-26 09:48:20 -0300

    Update
    Notificação com som

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View, Button } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+/* Manipulador de eventos de notificação */
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    /* Necessário para IOS */
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+          allowAnnouncements: true,
+        },
+      });
+    }
+    permissoesIos();
+
+    /* Ouvinte de evento para as notificações recebidas */
+    Notifications.addNotificationResponseReceivedListener((notificacao) => {
+      console.log(notificacao);
+    });
+    /* Ouvinte de evento para as respostas dadas às notificações */
+    Notifications.addNotificationReceivedListener((resposta) => {
+      console.log(resposta);
+    });
+  }, []);
+
+  const enviarMensagem = async () => {
+    const mensagem = {
+      title: "lembrete",
+      body: "Não se esqueça de tomar água!",
+      sound: "default",
+    };
+
+    await Notifications.scheduleNotificationAsync({
+      content: mensagem,
+      trigger: { seconds: 5 },
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <Text>Exemplo de sistema de notificações local</Text>
+        <View style={styles.botao}>
+          <Button title="Disparar Notificação" onPress={enviarMensagem} />
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  botao: {
+    borderRadius: 5,
+    borderWidth: 2,
+    margin: 6,
+    borderColor: "green",
   },
 });
